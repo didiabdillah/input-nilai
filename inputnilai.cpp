3eed6b0d9255bf3516ae06294cda18ee8470476c
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-05 14:03:10 +0700

    Menambahkan komentar

--- inputnilai.cpp
+++ inputnilai.cpp
@@ -2,30 +2,41 @@
 #include <math.h>
 #include <bits/stdc++.h>
 
+//Deklarasi namespace std
 using namespace std;
 
+//method untuk melakukan sorting
 void sorting(int nilai[], int x)
 {
+    //sorting nilai dalam array dari kecil ke besar
     sort(nilai, nilai + x);
+
+    //menampilkan nilai hasil sorting
     cout << "Nilai Setelah Di Sorting : ";
     for (int i = 0; i < x; ++i)
         cout << nilai[i] << " ";
 }
 
+//method utama
 int main()
 {
+    // deklarasi variabel x
     int x;
 
+    //input memasukkan nilai x
     cout << "Masukkan Jumlah Nilai : ";
     cin >> x;
 
+    // deklarasi variabel array nilai
     int nilai[x];
 
+    //input nilai sebanyak x
     for (int i = 0; i < x; i++)
     {
         cout << "Masukkan Nilai Ke " << i + 1 << " : ";
         cin >> nilai[i];
     }
 
+    //panggil method sorting
     sorting(nilai, x);
 }
