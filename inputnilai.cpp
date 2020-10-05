4973a3dfaa92cb6b300a3841996926d4d38e896b
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-05 13:57:05 +0700

    Membuat fitur sorting nilai di dalam array

--- inputnilai.cpp
+++ inputnilai.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+void sorting(int nilai[], int x)
+{
+    sort(nilai, nilai + x);
+    cout << "Nilai Setelah Di Sorting : ";
+    for (int i = 0; i < x; ++i)
+        cout << nilai[i] << " ";
+}
 
 int main()
 {
@@ -18,8 +27,5 @@ int main()
         cin >> nilai[i];
     }
 
-    for (int i = 0; i < x; i++)
-    {
-        cout << "Nilai " << i << "= " << nilai[i] << endl;
-    }
+    sorting(nilai, x);
 }
