86a703cce263428043e501adeb4772fad8d538ea
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-05 12:44:50 +0700

    Membuat Looping Masukkan Nilai Lalu Menampilkannya

--- inputnilai.cpp
+++ inputnilai.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World";
+    int x;
+
+    cout << "Masukkan Jumlah Nilai : ";
+    cin >> x;
+
+    int nilai[x];
+
+    for (int i = 0; i < x; i++)
+    {
+        cout << "Masukkan Nilai Ke " << i + 1 << " : ";
+        cin >> nilai[i];
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        cout << "Nilai " << i << "= " << nilai[i] << endl;
+    }
 }
